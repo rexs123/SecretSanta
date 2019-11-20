3fc1c918feda9761fb495d8945ae34239d08e20f
Rama Patel <me@rexsdev.com>
2019-11-20 09:03:50 -0500

    Profile modal, controller, and migrations

---
+++ database/migrations/2019_11_20_135824_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('group_id');
+            $table->string('full_name');
+            $table->string('address');
+            $table->string('address_opt')->nullable();
+            $table->string('city');
+            $table->string('state')->nullable();
+            $table->string('zip');
+            $table->string('country');
+            $table->text('bio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
