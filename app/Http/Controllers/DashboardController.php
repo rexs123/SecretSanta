8be433f86ca9392803f0a5e3ea8cea566d1fb323
Rama Patel <me@rexsdev.com>
2019-11-21 03:04:27 -0500

    Null matching correctly this time

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,7 +28,7 @@ class DashboardController extends Controller
         //return dd();
         return view('home', [
             'groups' => Auth::user()->groups,
-            'profile' => (Auth::user()->groups)? Profile::where('user_id', Auth::id())->where('group_id', Auth::user()->groups->first()->id)->first() : ''
+            'profile' => (Auth::user()->groups()->exists())? Profile::where('user_id', Auth::id())->where('group_id', Auth::user()->groups->first()->id)->first() : ''
         ]);
     }
 }
