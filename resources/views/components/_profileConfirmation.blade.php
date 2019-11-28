ed1cd95f6c1e2c9278801af422847d7cebbc68c8
Rama Patel <me@rexsdev.com>
2019-11-28 01:03:24 -0500

    I am enabling this cus people piss me off

--- resources/views/components/_profileConfirmation.blade.php
+++ resources/views/components/_profileConfirmation.blade.php
@@ -4,7 +4,7 @@
         @method('post')
         <button type="submit" class="btn btn-warning">Confirm your Profile</button>
     </form>
-    @elseif ($profile && Carbon::parse(now())->lessThan('2019-11-25 23:59:59'))
+    @elseif ($profile && Carbon::parse(now())->lessThan('2019-11-29 23:59:59'))
     <form action="{{ route('profile.destroy', ['profile' => $profile->id]) }}" method="POST">
         @csrf
         @method('delete')
